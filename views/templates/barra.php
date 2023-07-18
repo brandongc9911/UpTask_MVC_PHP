35130170af1972d2aafb983243ce719c01436b6c
brandongc9911 <brandongc9911@gmail.com>
2023-07-17 20:59:40 -0500

    Correccion template  barra menu

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,10 +1,12 @@
 <div class="barra-mobile">
     <h1>UpTask</h1>
+
     <div class="menu">
-        <img src="https://cdn-icons-png.flaticon.com/512/10080/10080458.png" alt="menu" id="mobile-menu">
+        <img id="mobile-menu" src="build/img/menu.svg" alt="imagen menu">
     </div>
 </div>
+
 <div class="barra">
-    <p>Hola: <span> <?php echo $_SESSION['nombre']?></span></p>
+    <p>Hola: <span><?php echo $_SESSION['nombre']; ?></span></p>
     <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
 </div>
